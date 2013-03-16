10c6fee83db6b95e53bceb72d96a8c67b8ba1473
Wei Dai <daiwei89@gmail.com>
2013-03-16 02:03:56 -0400

    MAP done; use map_est/run.sh to run on farm_ads dataset.

---
+++ map_est/run.sh
@@ -0,0 +1,7 @@
+time ./map_est --trainfile="../dataset/farm_ads_train.dat" \
+--testfile="../dataset/farm_ads_test.dat" \
+--sigma=0.05 --d=54877 --Ntrain=3314 --Ntest=829
+
+#time ./map_est --trainfile="../dataset/farm_ads_train_small.csv" \
+#--testfile="../dataset/farm_ads_test_small.csv" \
+#--sigma=0.05
